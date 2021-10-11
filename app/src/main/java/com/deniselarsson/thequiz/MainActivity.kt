a62a6d1a7b18f3e0819be854e9d863030a699896
dl <denisee.larsson@gmail.com>
2021-10-11 23:44:44 +0200

    save username from start page

--- app/src/main/java/com/deniselarsson/thequiz/MainActivity.kt
+++ app/src/main/java/com/deniselarsson/thequiz/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : AppCompatActivity() {
                     "Please enter your name", Toast.LENGTH_SHORT).show()
             }else{
                 val intent = Intent(this, QuizQuestionActivity::class.java)
-                //intent.putExtra(Constants.USER_NAME, et_name.text.toString())
+                intent.putExtra(Constants.USER_NAME, inputeName.text.toString())
                 startActivity(intent)
                 finish()
             }
